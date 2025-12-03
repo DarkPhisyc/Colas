589f8b712bdeeede1c0dd5780a66a5c8e26b7be7
DarkPhisyc <darkphisyc50@gmail.com>
2025-12-03 17:12:08 -0600

    Método para resolver las opersciones que necesitan factorial

--- colas.java
+++ colas.java
@@ -79,4 +79,11 @@ public class colas {
         } while (opcion != 4);
         sc.close();
     }
+    public static long factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return n * factorial(n - 1);
+        }
+    }
 }
